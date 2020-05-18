c0305b87aafdc15311850c625c8a479d26562991
Jason Ho <jason_ho@brown.edu>
2020-05-18 16:43:15 -0400

    Create ADEImage.py
    This will be class to house the ADEImage object that we will use later on.

---
+++ ADEImage.py
@@ -0,0 +1,15 @@
+# Imports
+
+
+
+
+# Class Declaration of ADEImage, the main object representing an ADE object.
+class ADEImage():
+    # TODO: Add the other things that we want to include here
+    def __init__(self):
+
+        # Check if the input is a filename or not:
+
+
+
+        # Fill out everything

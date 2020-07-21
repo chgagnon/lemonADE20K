a818597d80ec69ec49a63f126d680d3af99bd25c
chgagnon <47061354+chgagnon@users.noreply.github.com>
2020-07-21 13:30:13 -0400

    Resolve subdir name

--- lemonADE20K/__init__.py
+++
@@ -1,3 +0,0 @@
-from .funcs import refresh_csv_tables, get_filename_column, \
-get_foldername_column, get_typeset_column, get_scene_column, \
-get_list_of_object_names, get_object_image_matrix, get_images, get_filepaths
